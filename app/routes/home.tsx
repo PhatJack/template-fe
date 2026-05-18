d4ae2d1241bdc01fb87c50a06e5b49b6f9f79a6b
PhatJack <tienphat.ng693@gmail.com>
2026-05-18 19:38:34 +0700

    feat: update meta description for HomePage to reflect AI-powered editing capabilities

--- app/routes/home.tsx
+++ app/routes/home.tsx
@@ -3,7 +3,11 @@ import HomePage from "~/pages/home";
 export function meta() {
   return [
     { title: "Template.net | AI Generator for Fully Editable Assets" },
-    { name: "description", content: "Welcome to React Router!" },
+    {
+      name: "description",
+      content:
+        "Generate free templates, designs, documents, presentations, charts, whiteboards, and more in an all-in-one AI-powered editor. Turn prompts into fully editable, branded assets instantly. No signup needed",
+    },
   ];
 }
 
